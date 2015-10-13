dcad6eabbc258e8ccbb26eaff88496c025496ca9
Daniel Husar <dano.husar@gmail.com>
2015-10-13 20:59:19 +0100

    cleanup tests

--- test.js
+++ test.js
@@ -4,15 +4,21 @@ const fs = require('fs');
 const assert = require('assert');
 const proxyquire = require('proxyquire');
 const _ = require('lodash');
-const inventory = proxyquire('./index', {got: gotStub});
 
-function gotStub() {
-	return new Promise(resolve => {
-		resolve({
-			body: fs.readFileSync('./fixtures/success').toString()
+let stub = {};
+['success', 'private', 'error'].forEach(item => {
+	stub[item] = function () {
+		return new Promise(resolve => {
+			resolve({
+				body: fs.readFileSync(`./fixtures/${item}`).toString()
+			});
 		});
-	});
-}
+	};
+});
+
+const inventory = proxyquire('./index', {got: stub.success});
+const inventory2 = proxyquire('./index', {got: stub.private});
+const inventory3 = proxyquire('./index', {got: stub.error});
 
 it('Returnin item attributes', () => {
 	return inventory('fooo').then(data => {
@@ -30,11 +36,17 @@ it('Returnin item attributes', () => {
 	});
 });
 
-// it('Return empty array for private user', () => {
-// 	return inventory('234234234').then(data => {
-// 		assert.equal(data.length, 0);
-// 	});
-// });
+it('Return empty array for private user', () => {
+	return inventory2('234234234').then(data => {
+		assert.equal(data.length, 0);
+	});
+});
+
+it('Return empty array for non existing user', () => {
+	return inventory3('234234234').then(data => {
+		assert.equal(data.length, 0);
+	});
+});
 
 it('Username is mandatory', () => {
 	assert.throws(() => {
